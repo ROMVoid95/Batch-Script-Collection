871ab8c79b5ddbd857738726579fa91a6662d533
ROM <rom.void95@gmail.com>
2020-06-03 12:43:49 -0500

    translate colors

--- Batch Colors/Color.bat
+++ Batch Colors/Color.bat
@@ -1,14 +1,21 @@
-::Hello World !
 @echo OFF
 color 0A
-echo "Hello World !"
+echo "Hello World!"
 pause >nul
 
-::    0 = Noir          8 = Gris
-::    1 = Bleu          9 = Bleu clair
-::    2 = Vert          A = Vert clair
-::    3 = Bleu-gris     B = Cyan
-::    4 = Rouge         C = Rouge clair
-::    5 = Violet        D = Violet clair
-::    6 = Jaune         E = Jaune clair
-::    7 = Blanc         F = Blanc brillant
+::    0 = Black
+::    1 = Blue
+::    2 = Green
+::    3 = Bluish-Grey
+::    4 = Red
+::    5 = Purple
+::    6 = Yellow
+::    7 = White
+::    8 = Grey
+::    9 = Light Blue
+::    A = Light Green
+::    B = Cyan
+::    C = Light Red
+::    D = Light Purple
+::    E = Pale Yellow
+::    F = Brilliant white
